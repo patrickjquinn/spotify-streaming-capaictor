7cc57aeb96697819a28c399710474eff30f8fe0a
Pat Quinn <patrick.jm.quinn@gmail.com>
2022-05-17 12:51:21 +0100

    Added both frameworks

--- android/src/main/java/app/getradiant/spotify/SpotifyStreaming.java
+++ android/src/main/java/app/getradiant/spotify/SpotifyStreaming.java
@@ -1,6 +1,17 @@
 package app.getradiant.spotify;
 
 import android.util.Log;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+import java.text.SimpleDateFormat;
+
+
 import com.spotify.sdk.android.player.Config;
 import com.spotify.sdk.android.player.ConnectionStateCallback;
 import com.spotify.sdk.android.player.Connectivity;
@@ -13,10 +24,37 @@ import com.spotify.sdk.android.player.PlayerEvent;
 import com.spotify.sdk.android.player.Spotify;
 import com.spotify.sdk.android.player.SpotifyPlayer;
 
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
 public class SpotifyStreaming {
+    private String currentAccessToken = null;
+    private String currentClientId = null;
+    private SpotifyPlayer player = null;
+
+    private final Player.OperationCallback mOperationCallback = new Player.OperationCallback() {
+        @Override
+        public void onSuccess() {
+            System.out.println("OK!");
+        }
+
+        @Override
+        public void onError(Error error) {
+            System.out.println("ERROR:" + error);
+        }
+    };
 
     public String echo(String value) {
         Log.i("Echo", value);
         return value;
     }
+
+    public Void play(String uri, String token, String clientId) {
+        return null;
+    }
+
+
 }
